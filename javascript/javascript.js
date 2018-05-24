f0f631ee13d1cb74b1b72d1706f843a7115ceb66
HrAagesen <mikkel_aagesen@hotmail.com>
2018-05-24 13:19:40 +0200

    header

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,7 @@
 //Javascript/jqeury
+function openNav(){
+  document.getElementById("mynav").style.width = "100%";
+}
+function closeNav(){ /*javascript til at lukke menuen når der trykkes på x */
+  document.getElementById("mynav").style.width = "0%";
+}
