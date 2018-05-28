f13e4d7927d03057af7e25c8040d0678c0729fec
HrAagesen <mikkel_aagesen@hotmail.com>
2018-05-28 10:28:49 +0200

    slider

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,3 +5,37 @@ function openNav(){
 function closeNav(){ /*javascript til at lukke menuen når der trykkes på x */
   document.getElementById("mynav").style.width = "0%";
 }
+//  slider
+
+$(function() {
+
+  var width = 335;
+  var animationSpeed = 500;
+  var currentSlide = 1;
+
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+
+  // var control_next = $('control_next');
+
+  $('.control_next').click(function () {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function () {
+      currentSlide++;
+        if (currentSlide === $slides.length-5) {
+          currentSlide = 1;
+          $slideContainer.css('margin-left', 0);
+        }
+      });
+    });
+
+  $('.control_prev').click(function () {
+    $slideContainer.animate({'margin-left': '+='+width}, animationSpeed, function () {
+      currentSlide--;
+        if (currentSlide === -2) {
+          currentSlide = 4;
+          $slideContainer.css('margin-left', -1005);
+        }
+      });
+    });
+  });
